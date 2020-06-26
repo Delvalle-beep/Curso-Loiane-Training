e5cfc1923c3bd96915c0c6423d795fd970b17859
LadyWinehouse <vitoriastirling@gmail.com>
2020-06-25 23:52:05 -0300

    terminei o exercicio 9 que eu estava com dificuldades

--- src/com/company/aula14E15/ExercicioNove.java
+++ src/com/company/aula14E15/ExercicioNove.java
@@ -12,6 +12,37 @@ public class ExercicioNove {
         System.out.println("Digite o terceiro numero");
         int numC = scan.nextInt();
 
-        //VER RESOLUÇÃO
+        if(numA <= numB && numA <= numC && numB<= numC){
+            //numA é maior
+            //numB é maior
+            //numA < numB < numC
+            System.out.println(numC +"-"+numB+"-"+numA);
+        }else if(numA <= numB && numA <= numC && numC<= numB){
+            //numA é menor
+            //numB é maior
+            //numA < numC < numB
+            System.out.println(numB +"-"+numC+"-"+numA);
+        }else if(numB <= numA && numB <= numC && numA<= numB) {
+            //numB é menor
+            //numC é maior
+            //numB < numA < numC
+            System.out.println(numC + "-" + numA + "-" + numB);
+        }else if(numB <= numA && numB <= numC && numC<= numA){
+            //numB é menor
+            //numA é maior
+            //numB < numC < numA
+            System.out.println(numA +"-"+numC+"-"+numB);
+        }else if(numC <= numA && numC <= numB && numA<= numB){
+            //numC é menor
+            //numB é maior
+            //numC < numA < numB
+            System.out.println(numB +"-"+numA+"-"+numC);
+        }else if(numC <= numA && numC <= numB && numA <= numB){
+            //numC é menor
+            //numB é maior
+            //numC < numA < numB
+            System.out.println(numB +"-"+numC+"-"+numA);
+        }
+
     }
 }
